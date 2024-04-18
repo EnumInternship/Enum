a9d98b8e4b289381acafbc555e458ef8cf350f41
Oluwaseyi69 <oluwaseyitemitope69@gmail.com>
2024-04-18 17:05:10 +0100

    Project-Setup

---
+++ src/main/java/com/example/EnumProject/EnumProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.EnumProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnumProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EnumProject/EnumProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnumProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnumProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
